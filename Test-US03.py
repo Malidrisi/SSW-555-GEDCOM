feee9ddd7ecd36c50b61fcc6362c0449a206a2e1
ssetia93 <ssetia@stevens.edu>
2016-10-04 19:06:52 -0400

    acceptance test for user story 3
    acceptance test for user story 3

---
+++ Test-US03.py
@@ -0,0 +1,13 @@
+import unittest
+from US03 import birthBeforeDeath
+
+class TestMyFunctions(unittest.TestCase):
+
+    def test_birth_before_death(self):
+        birt_date = "1940-06-11"
+        deat_date = "1962-08-07"
+        self.assertTrue(True, deat_date > birt_date)
+        self.assertFalse(deat_date < birt_date, deat_date > birt_date)  
+        
+if __name__ == "__main__":
+    unittest.main(exit=False)  
