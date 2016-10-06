9ba5427932459b16c8f60134e4d3dac37529574e
Malidrisi <mahalidrisi@gmail.com>
2016-10-06 13:29:50 -0400

    removed open file path

--- Py04.py
+++ Py04.py
@@ -29,7 +29,7 @@ def getname():
         if person[0] == Item[1]:
             return person[1][5:]
 
-myfile = open('/Users/rafifarab/Desktop/SSW-555-GEDCOM/data.ged', 'r')
+myfile = open('data.ged', 'r')
 
 for l in myfile:
 	words = l.split()
@@ -112,7 +112,7 @@ for Fam in FamList:
         FamTable.add_row([Fam[0], hus, wife])			 
 #print FamTable, "\n"
 
-wr=open('/Users/rafifarab/Desktop/SSW-555-GEDCOM/output.txt','w')
+wr=open('output.txt','w')
 wr.write("\nIndividuals\n")
 wr.write(str(IndTable))
 wr.write("\n\nFamilies\n")
