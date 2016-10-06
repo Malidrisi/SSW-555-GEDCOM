18ad780b84ad8f5a8d216a7763c58c4633d44f0a
Malidrisi <mahalidrisi@gmail.com>
2016-10-06 14:11:06 -0400

    fixed bugs in US01

--- US01.py
+++ US01.py
@@ -1,28 +1,32 @@
+
+from datetime import date
 import datetime
+
 def date_before_current_date(IndList,wr):
-    list = []
+    l = []
     error_date = []
-    Today = str(datetime.datetime.today())
+    Today = date.today() 
     wr.write("\n\nUS01 - Date before current date")
-    for date in (IndList):
-        if date[3][:9] == "BIRT Date" and date[4][10:] == "Alive":
-            my_date = date[3][10:]
-            BirthDate = "Birthdate " + str(datetime.datetime.strptime(my_date,'%Y-%m-%d'))
-            list.append(BirthDate)
-        else:
-            my_date = date[4][10:]
-            DeathDate = "Deathdate " + str(datetime.datetime.strptime(my_date,'%Y-%m-%d'))
-            list.append(DeathDate)
-    count = 0
-    for index,i in enumerate(list):
-        if Today > list[index][10:]:
+    for d in (IndList):
+        bDate = d[3][10:]
+        l.append(bDate)
+        if d[4][10:] == "Alive":
             continue
         else:
-            error_date = error_date + [list[index][10:]]
+            dDate = d[4][10:]
+            l.append(dDate)
+
+    count = 0
+    
+    for index,i in enumerate(l):
+        if Today > datetime.datetime.strptime(l[index], '%Y-%m-%d').date():
+            continue
+        else:
+            error_date = error_date + [l[index]]
             count = count+1
     if count == 0:
         wr.write ("\nAll dates are correct")
     else:
         numb_of_ind = str(count)
-        output = "\n" + numb_of_ind + " date are after " + Today + " date " + str(error_date)
+        output = "\n" + numb_of_ind + " date are after " + str(Today)  + str(error_date)
         wr.write(output)
