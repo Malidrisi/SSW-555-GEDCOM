d68db0ffe1b3ea53c6fd52abcc06b7d13b5602df
Malidrisi <mahalidrisi@gmail.com>
2016-10-22 22:51:59 -0400

    fixed bugs in US36

--- US36.py
+++ US36.py
@@ -2,13 +2,13 @@ from datetime import date
 import datetime
 def ListRecentDeaths(IndList,wr):
     count= 0
-    wr.write("\n\nUS36 - ListRecentDeaths")
+    wr.write("\n\nUS36 - List Recent Deaths")
     for Ind in IndList:        
         if Ind[4][10:] == "Alive":
             pass
         else:
-            count= count + 1 
-            if (date.today() - datetime.datetime.strptime(Ind[4][10:], '%Y-%m-%d').date()).days <= 180:
+            if (date.today() - datetime.datetime.strptime(Ind[4][10:], '%Y-%m-%d').date()).days <= 30:
+                 count= count + 1 
                  output= "\n" + Ind[0] + "\t" +Ind[1][5:] + "\t"+ "died in " +Ind[4][10:]
                  wr.write(str(output))
             else:
